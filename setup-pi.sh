69ab4ff154f90bab4999995507b00be55c319224
Miner EU <MinerEU@users.noreply.github.com>
2014-05-01 12:26:52 +0100

    update setup script to be able to adapt to ubuntu 13.10 and debian(raspbian) wheezy

--- setup-pi.sh
+++ setup-pi.sh
@@ -8,17 +8,20 @@ fi
 apt-get update
 apt-get install -y lighttpd unzip wget openssl
 apt-get install -y  php5-common php5-cgi php5 
-apt-get install -y  php5-rrd libexpect-php5 php-auth-sasl php-mail php-net-smtp php-net-socket rrdtool
-apt-get install -y  libjansson4 libusb-1.0-0 ntpdate screen
+mkdir -p /etc/php5/conf.d/
+apt-get install -y  php5-rrd  php-auth-sasl php-mail php-net-smtp php-net-socket rrdtool php5-json
+apt-get install -y  libjansson4 libexpect-php5 libusb-1.0-0 ntpdate screen
 
 lighty-enable-mod fastcgi-php
+/etc/init.d/lighttpd restart
+
 mkdir /etc/lighttpd/certs
 cd /etc/lighttpd/certs
 openssl req -new -x509 -keyout lighttpd.pem -out lighttpd.pem -days 365 -nodes -subj "/C=US/ST=TEC/L=LONDON/O=DIS/CN=scripta.minereu.com" 
 chmod 400 lighttpd.pem
-/etc/init.d/lighttpd restart
+
 ssl_option=`grep "ssl.pemfile" /etc/lighttpd/lighttpd.conf`
-if [ "$ssl_option" == "" ]
+if [ "$ssl_option" == "" ];
 	then
 echo '$SERVER["socket"] == ":443" { ssl.engine = "enable" ssl.pemfile = "/etc/lighttpd/certs/lighttpd.pem" }' | tee -a  /etc/lighttpd/lighttpd.conf
 fi
@@ -32,13 +35,17 @@ cd /
 #install scripta
 #forked version from mox235 to udpate a few configuration to support the G-Blade 40 chip
 cd /tmp
-wget -O scriptaming.zip  https://github.com/MinerEU/scripta/archive/master.zip
+wget --no-check-certificate -O scriptaming.zip  https://github.com/MinerEU/scripta/archive/master.zip
 unzip -o scriptaming.zip
 cd scripta-master/
 
-rm -fr /var/www/*
-rm -fr /opt/scripta/*
-rm -fr /etc/cron.d/timeupdate
+backup_path=/opt/minereu_back$(date +"%Y%m%d%H%M%S")
+mkdir $backup_path
+
+mv -f /var/www/ $backup_path 2>/dev/null; true
+mv -f /opt/scripta/ $backup_path 2>/dev/null; true
+rm -fr /etc/cron.d/timeupdate 
+
 cp -fr usr etc opt var /
 chown -R  www-data /var/www
 chown -R  www-data /opt/scripta/
@@ -66,7 +73,7 @@ chmod +x /usr/local/bin/cgminer
 
 #fix the bug that pi stuck when mining 
 slub_debug_content=$(grep slub_debug< /boot/cmdline.txt)
-if [ "$slub_debug_content" == "" ]
+if [ "$slub_debug_content" == "" ] && [ ! "$is_raspbian" == "" ]
 	then
 	#append  slub_debug=FPUZ to existing content and write back 
 	 echo  "$(cat /boot/cmdline.txt) slub_debug=FPUZ" > /boot/cmdline.txt
